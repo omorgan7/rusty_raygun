25d4533ca88a57f21c24308b6229ed032c54667e
Owen Morgan <omorgan7@me.com>
2019-12-24 21:15:58 +0100

    latest version w/ monte carlo.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,41 @@ use std::io::Write;
 use std::f32::consts::PI;
 use std::f32::INFINITY;
 
+extern crate rand;
+use rand::Rng;
+use rand::rngs::ThreadRng;
+
 mod vector3;
 use vector3::Vector3;
+
+struct RandomEngine {
+    rng: ThreadRng
+}
+
+static MAX_ITERATIONS : i32 = 4;
+
+impl RandomEngine {
+    fn hemispherical_ray(&mut self, centre: Vector3, normal: Vector3) -> Vector3 {
+
+        let nz = normal;
+        let nx = (if (nz.x).abs() > (nz.y).abs() { Vector3{x: nz.z, y: 0.0, z: -nz.x} } else { Vector3{x: 0.0, y: -nz.z, z: nz.y} }).normalised();
+        let ny = nz.cross(nx);
+    
+        let u1 : f32 = self.rng.gen();
+        let u2 : f32 = self.rng.gen();
+    
+        let phi = 2.0 * PI * u2;
+        let sin_theta = (1.0 - u1 * u1).sqrt();
+
+        let random = Vector3{x: sin_theta * phi.cos(), y: u1, z: sin_theta * phi.sin()};
+
+        Vector3 {
+            x: random.x * nx.x + random.y * nz.x + random.z * ny.x,
+            y: random.x * nx.y + random.y * nz.y + random.z * ny.y,
+            z: random.x * nx.z + random.y * nz.z + random.z * ny.z,
+        }
+    }
+}
 
 fn write_to_ppm(filename : &str, image : &[u8], width : i32, height : i32) -> std::io::Result<()> {
 
@@ -83,23 +116,30 @@ impl RayHit {
 }
 
 trait Traceable {
-    fn trace(&self, ray_origin: Vector3, ray_direction: Vector3) -> Option<RayHit>;
-    fn colour(&self, ray_direction: Vector3, hit_position: Vector3, scene: &Scene, objects: &[&dyn Traceable]) -> Vector3;
+    fn trace(&self,
+        ray_origin: Vector3,
+        ray_direction: Vector3) -> Option<RayHit>;
+
+    fn colour(&self,
+        rng : &mut RandomEngine,
+        iteration : i32,
+        ray_direction: Vector3,
+        hit_position: Vector3,
+        scene: &Scene,
+        objects: &[&dyn Traceable]) -> Vector3;
 }
 
 #[derive(Clone)]
 struct Sphere {
     origin : Vector3,
     colour : Vector3,
-    radius : f32,
-    diffuseness : f32,
-    specularity : f32
+    radius : f32
 }
 
 struct Scene {
-    sun_origin : Vector3,
     spheres: Vec<Sphere>,
     triangles: Vec<Triangle>,
+    lights: Vec<Triangle>
 }
 
 #[derive(Clone)]
@@ -110,13 +150,11 @@ struct Triangle {
     edge_cb : Vector3,
     edge_ac : Vector3,
     colour : Vector3,
-    plane_offset : f32,
-    diffuseness : f32,
-    specularity : f32
+    plane_offset : f32
 }
 
 impl Triangle {
-    fn new(vertices : [Vector3; 3], colour : Vector3, diffuseness : f32, specularity : f32) -> Triangle {
+    fn new(vertices : [Vector3; 3], colour : Vector3) -> Triangle {
         let a = vertices[0];
         let b = vertices[1];
         let c = vertices[2];
@@ -136,9 +174,7 @@ impl Triangle {
             edge_cb: edge_cb,
             edge_ac: edge_ac,
             colour : colour,
-            plane_offset: plane_offset,
-            diffuseness : diffuseness,
-            specularity : specularity
+            plane_offset: plane_offset
         }
     }
 
@@ -182,13 +218,33 @@ impl Traceable for Triangle {
         Some(RayHit::from_intersection(ray_length, intersection_point))
     }
 
-    fn colour(&self, ray_direction: Vector3, hit_position: Vector3, scene: &Scene, objects: &[&dyn Traceable]) -> Vector3 {
+    fn colour(&self, rng : &mut RandomEngine, iteration : i32, ray_direction: Vector3, hit_position: Vector3, scene: &Scene, objects: &[&dyn Traceable]) -> Vector3 {
 
-        let shadow =  -(hit_position - scene.sun_origin);
-        let ray_direction = shadow.normalised();
-        let ray_length = shadow.length();
+        let surface_normal = self.normal;
+        let shadow = rng.hemispherical_ray(hit_position, surface_normal);
 
-        let ambient_colour = self.colour * (1.0 - self.diffuseness);
+        let colour = self.colour;
+
+        let eps : f32 = 1e-2;
+
+        // intersect with the nearest light
+        let mut light_length : f32 = INFINITY;
+        let mut light_hit : Option<(&dyn Traceable, Vector3)> = None;
+
+        for light in scene.lights.iter() {
+            if let Some(hit) = light.trace(hit_position, shadow) {
+                if hit.ray_length + eps < light_length {
+                    light_length = hit.ray_length;
+
+                    // lights don't have different colour or intensity for now.
+                    light_hit = Some((light, Vector3::ones() * 100.0));
+                }
+            }
+        }
+
+        let mut object_length : f32 = INFINITY;
+        let mut object_hit : Option<(&dyn Traceable, Vector3)> = None;
+
         for object in objects.iter() {
             let self_ptr = self as *const Triangle as *const usize;
             let object_ptr = *object as *const dyn Traceable as *const usize;
@@ -197,21 +253,39 @@ impl Traceable for Triangle {
                 continue;
             }
 
-            // floating point imprecision fudge
-            let eps : f32 = 1e-2;
+            if let Some(hit) = object.trace(hit_position, shadow) {
+                if hit.ray_length + eps < object_length {
+                    object_length = hit.ray_length;
 
-            if let Some(hit) = object.trace(hit_position, ray_direction) {
-                if hit.ray_length + eps < ray_length {
-                    return ambient_colour;
+                    // lights don't have different colour or intensity for now.
+                    object_hit = Some((*object, hit.intersection_point));
                 }
             }
         }
 
-        let diffuse_response = self.normal.dot(&ray_direction);
-        if (diffuse_response < 0.0) {
-            return ambient_colour;
+        if iteration == MAX_ITERATIONS {
+            // our scene is a closed system
+            // so this _should_ be impossible.
+            // convert to assertion later on.
+            return Vector3::zeroes();
         }
-        Vector3::ones() * diffuse_response * self.diffuseness + ambient_colour
+
+        if let Some((light, colour)) = light_hit {
+            if light_length < object_length {
+                return colour;
+            }
+        }
+
+        match object_hit {
+            None => {
+                return Vector3::zeroes();
+            }
+
+            Some((object, new_hit_position)) => {
+                let diffuse_response =  self.colour * 0.5 * surface_normal.dot(&shadow);
+                self.colour(rng, iteration + 1, shadow, new_hit_position, scene, objects) * diffuse_response
+            }
+        }
     }
 }
 
@@ -239,11 +313,32 @@ impl Traceable for Sphere {
         return if ray_length < 0.0 { None } else { Some(RayHit::new(ray_length, ray_origin, ray_direction)) };
     }
 
-    fn colour(&self, ray_direction: Vector3, hit_position: Vector3, scene: &Scene, objects: &[&dyn Traceable]) -> Vector3 {
+    fn colour(&self, rng : &mut RandomEngine, iteration : i32, ray_direction: Vector3, hit_position: Vector3, scene: &Scene, objects: &[&dyn Traceable]) -> Vector3 {
 
-        let shadow =  -(hit_position - scene.sun_origin);
-        let ray_direction = shadow.normalised();
-        let ray_length = shadow.length();
+        let surface_normal = (self.origin - hit_position).normalised();
+        let shadow = rng.hemispherical_ray(hit_position, surface_normal);
+
+        let colour = self.colour;
+
+        let eps : f32 = 1e-2;
+
+        // intersect with the nearest light
+        let mut light_length : f32 = INFINITY;
+        let mut light_hit : Option<(&dyn Traceable, Vector3)> = None;
+
+        for light in scene.lights.iter() {
+            if let Some(hit) = light.trace(hit_position, shadow) {
+                if hit.ray_length + eps < light_length {
+                    light_length = hit.ray_length;
+
+                    // lights don't have different colour or intensity for now.
+                    light_hit = Some((light, Vector3::ones() * 100.0));
+                }
+            }
+        }
+
+        let mut object_length : f32 = INFINITY;
+        let mut object_hit : Option<(&dyn Traceable, Vector3)> = None;
 
         for object in objects.iter() {
             let self_ptr = self as *const Sphere as *const usize;
@@ -253,29 +348,39 @@ impl Traceable for Sphere {
                 continue;
             }
 
-            let eps : f32 = 1e-2;
+            if let Some(hit) = object.trace(hit_position, shadow) {
+                if hit.ray_length + eps < object_length {
+                    object_length = hit.ray_length;
 
-            if let Some(hit) = object.trace(hit_position, ray_direction) {
-                if hit.ray_length + eps < ray_length {
-                    return self.ambient_colour();
+                    // lights don't have different colour or intensity for now.
+                    object_hit = Some((*object, hit.intersection_point));
                 }
             }
         }
 
-        let surface_normal = (hit_position - self.origin).normalised();
-        let diffuse_response = surface_normal.dot(&ray_direction);
-        if (diffuse_response < 0.0) {
-            return self.ambient_colour()
+        if iteration == MAX_ITERATIONS {
+            // our scene is a closed system
+            // so this _should_ be impossible.
+            // convert to assertion later on.
+            return Vector3::zeroes();
         }
-        Vector3::ones() * diffuse_response * self.diffuseness + self.ambient_colour()
-    }
-}
 
-impl Sphere {
-    fn ambient_colour(&self) -> Vector3 {
-        let ambient_coefficient = 1.0 - self.diffuseness - self.specularity;
+        if let Some((light, colour)) = light_hit {
+            if light_length < object_length {
+                return colour;
+            }
+        }
 
-        self.colour * ambient_coefficient
+        match object_hit {
+            None => {
+                return Vector3::zeroes();
+            }
+
+            Some((object, new_hit_position)) => {
+                let diffuse_response =  self.colour * 0.5 * surface_normal.dot(&shadow);
+                self.colour(rng, iteration + 1, shadow, new_hit_position, scene, objects) * diffuse_response
+            }
+        }
     }
 }
 
@@ -294,62 +399,60 @@ impl Scene {
             [Vector3{x: 10.0,  y: -10.0,  z: 10.0}, Vector3{x: 10.0, y: -10.0, z: -10.0}, Vector3{x: -10.0,  y: -10.0, z: -10.0}],
             [Vector3{x: 10.0, y: -10.0, z: 10.0}, Vector3{x: -10.0,  y: -10.0,  z: -10.0}, Vector3{x: -10.0,  y: -10.0, z: 10.0}],
             [Vector3{x: 10.0,  y: -10.0,  z: -10.0}, Vector3{x: -10.0,  y: 10.0, z: -10.0}, Vector3{x: -10.0, y: -10.0, z: -10.0}],
-            [Vector3{x: -10.0, y: 10.0, z: -10.0}, Vector3{x: 10.0,  y: -10.0,  z: -10.0}, Vector3{x: 10.0,  y: 10.0, z: -10.0}],
+            [Vector3{x: -10.0, y: 10.0, z: -10.0}, Vector3{x: 10.0,  y: -10.0,  z: -10.0}, Vector3{x: 10.0,  y: 10.0, z: -10.0}]
+        ];
+
+        let light_verts : [[Vector3; 3]; 1] = [
+            [Vector3{x: 2.0,  y: 9.0,  z: 2.0}, Vector3{x: -2.0, y: 9.0, z: 2.0}, Vector3{x: -2.0,  y: 9.0, z: -2.0}],
+            // [Vector3{x: 2.0, y: 9.0, z: -2.0}, Vector3{x: 2.0,  y: 9.0,  z: 2.0}, Vector3{x: -2.0,  y: 9.0, z: -2.0}]
         ];
 
         let colour = Vector3{x: 0.8, y: 0.8, z: 0.0};
 
-        let triangles = vertices.iter().map(| v | Triangle::new(*v, colour, 0.7, 0.1)).collect();
+        let to_tri = | v: &[Vector3; 3] | { Triangle::new(*v, colour) };
+
+        let triangles = vertices.iter().map(to_tri).collect();
+        let lights = light_verts.iter().map(to_tri).collect();
 
         let spheres = [
             Sphere {
                 origin: Vector3{x: 0.0, y: -8.0, z: 0.0},
                 colour: Vector3{x: 0.2, y: 0.8, z: 0.8},
-                radius: 2.0,
-                diffuseness: 0.7,
-                specularity: 0.1,
+                radius: 2.0
             },
             Sphere {
                 origin: Vector3{x: 1.0, y: -4.0, z: 10.0},
                 colour: Vector3{x: 0.2, y: 0.7, z: 0.1},
-                radius: 6.0,
-                diffuseness: 0.7,
-                specularity: 0.1,
+                radius: 6.0
             },
             Sphere {
                 origin: Vector3{x: 3.0, y: -8.6, z: 2.0},
                 colour: Vector3{x: 0.5, y: 0.4, z: 0.2},
-                radius: 1.4,
-                diffuseness: 0.7,
-                specularity: 0.1,
+                radius: 1.4
             },
             Sphere {
                 origin: Vector3{x: -4.0, y: -6.8, z: -1.0},
                 colour: Vector3{x: 0.1, y: 0.5, z: 0.15},
-                radius: 3.2,
-                diffuseness: 0.7,
-                specularity: 0.1,
+                radius: 3.2
             },
             Sphere {
                 origin: Vector3{x: 5.0, y: -9.4, z: 0.5},
                 colour: Vector3{x: 0.7, y: 0.2, z: 0.5},
-                radius: 0.6,
-                diffuseness: 0.7,
-                specularity: 0.1,
+                radius: 0.6
             }
         ];
 
         Scene {
-            sun_origin: Vector3{x: 0.0, y: 8.0, z: -7.0},
             spheres: spheres.to_vec(),
             triangles: triangles,
+            lights: lights
         }
     }
 }
 
 fn main() {
-    let width = 1280;
-    let height = 720;
+    let width = 512;
+    let height = 288;
     let channels = 3;
 
     let field_of_view : f32 = 90.0;
@@ -376,15 +479,20 @@ fn main() {
     scene.triangles.iter().for_each(| tri | objects.push(tri as &dyn Traceable));
     scene.spheres.iter().for_each(| s | objects.push(s as &dyn Traceable));
 
+    let thread_count : usize = 8;
+
+    let mut rng = RandomEngine{rng: rand::thread_rng()};
+
     for pixel_count in (0..(width * height * channels)).step_by(channels) {
         let x = ((pixel_count / channels) % width) as f32;
         let y = ((pixel_count / channels) / width) as f32;
+
+        println!("Processing: {} {}", x, y);
 
         let ray_direction = (camera.image_plane_edge - camera.left * normalised_width * x + camera.image_plane_center - camera.up * normalised_height * y - camera.origin).normalised();
 
         let mut max_dist = INFINITY;
         let mut object_hit : Option<(&dyn Traceable, Vector3)> = None;
-
 
         for object in objects.iter() {
             if let Some(ray_hit) = object.trace(camera.origin, ray_direction) {
@@ -396,9 +504,16 @@ fn main() {
         }
 
         let mut colour = Vector3::zeroes();
-        if let Some((object, intersection_point)) = object_hit {
-            colour = object.colour(ray_direction, intersection_point, &scene, &objects);
+
+        let count = 1000;
+
+        for i in 0..count {
+            if let Some((object, intersection_point)) = object_hit {
+                colour = colour + object.colour(&mut rng, 0, ray_direction, intersection_point, &scene, &objects);
+            }
         }
+
+        colour = colour / (count as f32);
 
         let (r, g, b) = colour.to_rgb();
 
